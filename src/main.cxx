51f93cbf59757d0522a5b544c0934643fe3b3b93
Sio <sioclya@mailbox.tu-berlin.de>
2022-07-09 23:16:13 +0200

    unity build

--- src/main.cxx
+++ src/main.cxx
@@ -1,5 +1,12 @@
-#include "primary/primary.h"
-#include "secondary/secondary.h"
+#include <stdio.h>
+#include <string>
+#include <stlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "lib.cxx"
+#include "primary.cxx"
+#include "secondary.cxx"
 
 int main(int argc, char** argv) {
     // TODO: depending on argv[1] (==prepare || == primary || == secondary), either invoke primary or secondary with other provided args
